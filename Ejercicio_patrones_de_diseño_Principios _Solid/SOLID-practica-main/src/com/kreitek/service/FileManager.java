6ad3109a7db3742240ef82bd0f5fa71b3a2cfcad
Duhert02 <81563377+Duhert02@users.noreply.github.com>
2022-04-24 20:02:36 +0200

    Cuarto cambio
    Eliminacion de un metodo que no se utiliza y de comentarios

--- Ejercicio_patrones_de_diseño_Principios _Solid/SOLID-practica-main/src/com/kreitek/service/FileManager.java
+++ Ejercicio_patrones_de_diseño_Principios _Solid/SOLID-practica-main/src/com/kreitek/service/FileManager.java
@@ -22,20 +22,6 @@ public class FileManager {
         return totalSize;
     }
 
-    public static int calculateSizeList(List<FileSystemItem> files) {
-        int totalSize = 0;
 
-        for(FileSystemItem item : files) {
-            if (item instanceof File) {
-                totalSize += item.getSize();
-            } else if (item instanceof Directory) {
-                totalSize += calculateSizeList(item.listFiles());
-            }
-        }
 
-        return totalSize;
-    }
-
-    // Aquí habría otros métodos para gestionar ficheros y directorios:
-    // Crear ficheros, mover ficheros, eliminar ficheros, etc.
 }
